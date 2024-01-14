3b65a085e31e9bfe773fda5eba6af66c3bdc4981
Krish Bhuva <124707901+196170303023@users.noreply.github.com>
2024-01-14 19:46:56 +0530

    Add files via upload

---
+++ 03_objects.js
@@ -0,0 +1,37 @@
+// object are the only non primitive data type in java script.
+// an object in javascript 
+//objects are key value pairs
+
+let bio = 
+{
+    name:"krish",
+    age:18,
+    college:"parul",
+    division:"6b24"
+
+}
+// to add any property inside object we use
+bio.semester=6
+console.log(bio)
+// to update value of any property inside the object 
+bio.age=19
+console.log(bio) 
+
+// or you can use bracket noation also to add and update properties inn obj
+// to add any property inside object we use
+bio['semester']=67
+console.log(bio)
+// to update value of any property inside the object 
+bio['age']=30
+console.log(bio) 
+
+let cv =
+{
+    skill:"java",
+    krish:"bhuva",
+    exprience:3,
+    salary:40000,
+}
+// there are two type of notation in java script to fetch the values the onject
+//bracket notation and dot noation 
+ 
