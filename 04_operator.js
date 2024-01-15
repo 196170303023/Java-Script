3f68641545939b6625aa925e64edcea1eef3a95a
Krish Bhuva <124707901+196170303023@users.noreply.github.com>
2024-01-15 18:08:15 +0530

    Add files via upload

---
+++ 04_operator.js
@@ -0,0 +1,53 @@
+//arithmetc operators 
+ {
+let a = 2;
+let b = 4;
+
+console.log("a + b  = ", a + b);
+console.log("a - b  = ", a - b);
+console.log("a * b  = ", a * b);
+console.log("a / b  = ", a / b);
+console.log("a % b  = ", a % b);
+console.log("a++  = ", a++);
+// there are two types of increments in js preincriment and postincriment 
+// pre inc, will first increment then print the value of a 
+// post inc, will print the value of a then increment 
+//you have to print a again if you want incremented value 
+console.log("++a =", ++a);
+console.log("a-- = ", a--);
+console.log("--a = ", --a);
+console.log("a ** b = ", a ** b);
+}
+
+// assignment operaters 
+
+let b = 5
+b+=5//same as b = b+5
+b-=5//same as b = b-5
+b*=5//same as b = b*5
+b/=5//same as b = b/5
+b%=5//same as b = b%5
+b**=5//same as b = b**5
+
+// comparision operators
+/*
+
+==      equals to 
+!=      not equals to
+===     compiler cheack both value as well as data type return true if both are correct 
+!==     not equal value or not equal type 
+>       greater than/<//less than 
+>=      greater than equal to
+<=      lesser than equal to
+ 
+                        //logical operator
+                        
+&&   true and true will be true and all other possibilites are false
+||   false and false will be false 
+!    return opposite value
+
+*/
+
+ 
+
+
