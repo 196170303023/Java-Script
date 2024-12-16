68a945f52cb7066cf8b8675154f1990288e757db
Krish Bhuva <bhuvakrish03@gmail.com>
2024-12-17 03:14:16 +0530

    new

---
+++ arrays2.js
@@ -0,0 +1,30 @@
+// // //question 1
+// // // function abc (){
+// // //     console.log(abc.xyz);
+// // // }
+
+// // // abc()
+// // // abc.xyz = 400
+// // // abc.xyz = 200
+// // // abc()
+
+// // //question2
+// // // const number = [1,2,3,4]
+// // // number[10]=500
+// // // console.log(number);
+
+
+// // //question 3
+// // console.log(typeof typeof 100);
+
+// //question 4
+
+// const arr =[..."krish"]
+// console.log(arr);
+
+// //question 5
+// console.log(parseInt("10+2"));
+// console.log(parseInt("7FM"));
+// console.log(parseInt("f7f"));
+
+
